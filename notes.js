dc551884d914b490da9ec89587fb6d9b111f1aaf
Valerie Jane <valeriejanethoma@gmail.com>
2018-02-09 08:52:01 -0500

    add yargs

--- notes.js
+++ notes.js
@@ -1,13 +1,21 @@
 console.log('Starting notes.js')
 
-// module.exports.addNote = () => {
-//   console.log('addNote');
-//   return 'New note';
-// };
-
-module.exports.add = (a, b) =>{
-  return a + b
+var addNote = (title, body) => {
+  console.log('Adding note', title, body)
+}
+var getAll = () => {
+  console.log('Getting all notes');
+}
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+var removeNote = (title) => {
+  console.log('Removing note', title);
 };
 
-
-// console.log(module);
+module.exports = {
+  addNote,
+  getAll, 
+  getNote, 
+  removeNote
+};
