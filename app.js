b349da75441e97af2f4b28a9d59c7fd64aab1826
Valerie Jane <valeriejanethoma@gmail.com>
2018-02-10 22:38:53 -0500

    add 'list' command

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -9,9 +7,9 @@ const notes = require('./notes.js')
 
 var argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-// console.log('Process', process.argv);
-console.log('Yargs', argv);  
+// console.log('Command: ', command);
+// // console.log('Process', process.argv);
+// console.log('Yargs', argv);  
 
 if (command === 'add') {
   console.log('Adding new note');
@@ -23,7 +21,9 @@ if (command === 'add') {
     console.log('Note title already exists')
   }
 }else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).}`);
+  allNotes.forEach((note)=> notes.logNote(note));
 }else if (command === 'read'){
   var note = notes.getNote(argv.title)
   if (note) {
