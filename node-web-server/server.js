ae5de061a1d53386174a0ae5e2fa3ed3025f8cba
Valerie Jane <valeriejanethoma@gmail.com>
2018-02-25 10:50:04 -0500

    add partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,24 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello, express!</h1>');
-  res.send({
-    name: 'Valerie',
-    likes: [
-      'Reading',
-      'Writing',
-      'Hiking',
-      'Televison'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
+
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
